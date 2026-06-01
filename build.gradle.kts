62cbe347dda234f5a2bae0e622fd485c206c79b4
aayush-pandit <dwggaming0@gmail.com>
2026-06-01 21:38:16 +0530

    Update build.gradle.kts
    Yes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-  alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
+
 
 android {
   namespace = "com.example"
